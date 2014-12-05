5f1add58b83e2acacc123200328cc60b2ecd2fa4
sushiogoto <yoshio.goto@gmail.com>
2014-12-05 17:52:48 +0800

    Initialize repo, add packages and github login

---
+++ server/config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+  service: "github"
+});
+
+ServiceConfiguration.configurations.insert({
+  service: "github",
+  clientId: "745428022e3cfb98483d",
+  secret: "5d95ae63ace4e66256e8965126043a04ba2b00f5"
+});
