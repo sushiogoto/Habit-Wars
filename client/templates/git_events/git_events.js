98c70dc9e77f11d92155399ed2f453d0663af4c8
sushiogoto <yoshio.goto@gmail.com>
2014-12-08 22:26:04 +0800

    Partially add apsfuasnfp

--- client/templates/git_events/git_events.js
+++
@@ -1,9 +0,0 @@
-Template.gitEvents.events({
-  'click .getgit': function(e) {
-    e.preventDefault();
-    Meteor.call('updateGitRecord', function (error, result) {
-    });
-  }
-});
-
-console.log("TEST AGAIN")
