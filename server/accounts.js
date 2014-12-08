1511e26977efe45908938dc0491b726b12826c44
sushiogoto <yoshio.goto@gmail.com>
2014-12-08 10:40:12 +0800

    Partially complete account-merge package added

--- server/accounts.js
+++ server/accounts.js
@@ -1,41 +1,41 @@
-Accounts.onCreateUser(function (options, user) {
-  var accessToken = user.services.github.accessToken,
-    result,
-    profile;
+// Accounts.onCreateUser(function (options, user) {
+//   var accessToken = user.services.github.accessToken,
+//     result,
+//     profile;
 
-  result = Meteor.http.get("https://api.github.com/user", {
-    params: {
-      access_token: accessToken
-    },
-    headers: {"User-Agent": "Meteor/1.0"}
-  });
+//   result = Meteor.http.get("https://api.github.com/user", {
+//     params: {
+//       access_token: accessToken
+//     },
+//     headers: {"User-Agent": "Meteor/1.0"}
+//   });
 
-  if (result.error)
-    throw result.error;
+//   if (result.error)
+//     throw result.error;
 
-  profile = _.pick(result.data,
-    "login",
-    "name",
-    "avatar_url",
-    "url",
-    "company",
-    "blog",
-    "location",
-    "email",
-    "bio",
-    "html_url");
+//   profile = _.pick(result.data,
+//     "login",
+//     "name",
+//     "avatar_url",
+//     "url",
+//     "company",
+//     "blog",
+//     "location",
+//     "email",
+//     "bio",
+//     "html_url");
 
-  user.profile = profile;
+//   user.profile = profile;
 
-  Characters.insert({
-    userId: user._id,
-    name: "Megaman",
-    avatar_url: "http://www.gearfuse.com/wp-content/uploads/2010/06/retro-gaming-3d-3.gif",
-    health: 100,
-    strength: 10,
-    intelligence: 10,
-    dexterity: 10
-  });
+//   Characters.insert({
+//     userId: user._id,
+//     name: "Megaman",
+//     avatar_url: "http://www.gearfuse.com/wp-content/uploads/2010/06/retro-gaming-3d-3.gif",
+//     health: 100,
+//     strength: 10,
+//     intelligence: 10,
+//     dexterity: 10
+//   });
 
-  return user;
-});
+//   return user;
+// });
--- server/config.js
+++ server/config.js
@@ -1,9 +1,15 @@
-ServiceConfiguration.configurations.remove({
-  service: "github"
-});
+// ServiceConfiguration.configurations.remove({
+//   service: ["github", "facebook"]
+// });
 
-ServiceConfiguration.configurations.insert({
-  service: "github",
-  clientId: "745428022e3cfb98483d",
-  secret: "5d95ae63ace4e66256e8965126043a04ba2b00f5"
-});
+// ServiceConfiguration.configurations.insert({
+//   service: "github",
+//   clientId: "745428022e3cfb98483d",
+//   secret: "5d95ae63ace4e66256e8965126043a04ba2b00f5"
+// });
+
+// ServiceConfiguration.configurations.insert({
+//   service: "facebook",
+//   clientId: "833737010003369",
+//   secret: "5ae86e2d59f7277cba7ca6730951e230"
+// });
