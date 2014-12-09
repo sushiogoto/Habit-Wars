7933e97f0109623f3f56698acce67b550e34c261
sushiogoto <yoshio.goto@gmail.com>
2014-12-09 13:14:44 +0800

    Finish listing user git commits

--- client/templates/character/character_index.js
+++ client/templates/character/character_index.js
@@ -11,6 +11,18 @@ Template.characterIndex.events({
 
 Template.characterIndex.helpers({
   commits: function() {
+    git = GitRecords.findOne({userId: Meteor.user()._id});
+    git_array = _.map(git.git_record, function(commits, key) {
+      return key + ": " + commits;
+    });
+    // date = new Date();
+    // year = date.getUTCFullYear();
+    // month = date.getUTCMonth() + 1;
+    // day = date.getUTCDate();
+    // timeNow = year + "-" + month + "-" + day;
+    // for(var i = 0; i < git.git_record.length; i++) {
 
+    // }
+    return git_array;
   }
 });
