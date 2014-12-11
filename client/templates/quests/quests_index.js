ee5924b5a24701ae5e54a076da3f7cb291523456
sushiogoto <yoshio.goto@gmail.com>
2014-12-11 21:37:54 +0800

    Add user damaged after failing quest at end of day.

--- client/templates/quests/quests_index.js
+++ client/templates/quests/quests_index.js
@@ -4,3 +4,20 @@ Template.questsIndex.events({
     Meteor.call('questInit', 'easy', function (error, result) {});
   }
 });
+
+Template.questsIndex.rendered = function() {
+  var user = Meteor.user();
+  var quest = Quests.findOne({userId: user._id, active: true});
+
+
+  // add a day
+  if(quest) {
+    var questEndDate = moment(quest.startDate).add(quest.questDuration , 'd').format("YYYY-MM-DD");
+    var dateToday = moment();
+    var endQuest = moment(questEndDate).isBefore(dateToday);
+    if(endQuest) {
+      Meteor.call('questEnd', function (error, result) {});
+    }
+  }
+
+};
