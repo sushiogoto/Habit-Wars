2c697c33ae1229e226509f680aa7c5c91c9330b0
sushiogoto <yoshio.goto@gmail.com>
2014-12-17 15:03:52 +0800

    Finish item draggable logic and almost finish popover for individual item information.

--- client/templates/items/items_inventory.js
+++ client/templates/items/items_inventory.js
@@ -51,49 +51,59 @@ Template.itemsInventory.rendered = function () {
   var character = Characters.findOne({userId: Meteor.userId()});
   var inventory = Inventories.findOne({characterId: character._id});
 
+
   Tracker.autorun(function () {
-  $('.item').draggable({
-    snap: ".inventory-table-cell",
-    snapMode: "inner",
-    snapTolerance: 30,
-    stop: function(event, ui) {
-      itemsGridDep.depend();
-      inventory = Inventories.findOne({characterId: character._id});
-      // var offset = $('.inventory-table').offset();
-      // var left = ui.position.left;
-      // var top = ui.position.top;
-      var itemId = $(event.target).data('item-id');
-      var invTableOffsetTop = $('.inventory-table').offset().top;
-      var invTableOffsetLeft = $('.inventory-table').offset().left;
-      var cursorPosTop = event.pageY;
-      var cursorPosLeft = event.pageX;
-      // flatItemsGridArray = _.flatten(inventory.itemsGrid);
+    console.log('draggable');
+    itemsGridDep.depend();
+    $('.item').draggable({
+      snap: ".inventory-table-cell",
+      snapMode: "inner",
+      snapTolerance: 15,
+      stop: function(event, ui) {
+        inventory = Inventories.findOne({characterId: character._id});
+        // var offset = $('.inventory-table').offset();
+        // var left = ui.position.left;
+        // var top = ui.position.top;
+        var itemId = $(event.target).data('item-id');
+        var invTableOffsetTop = $('.inventory-table').offset().top;
+        var invTableOffsetLeft = $('.inventory-table').offset().left;
+        var cursorPosTop = event.pageY;
+        var cursorPosLeft = event.pageX;
+        // flatItemsGridArray = _.flatten(inventory.itemsGrid);
 
-      var convertPosToDivArrayPos = function(position) {
-        return Math.floor(position/50);
-      };
+        var convertPosToDivArrayPos = function(position) {
+          return Math.floor(position/30);
+        };
 
-      var toRow = convertPosToDivArrayPos(cursorPosTop - invTableOffsetTop);
-      var toCol = convertPosToDivArrayPos(cursorPosLeft - invTableOffsetLeft);
-      if(toRow < 0 || toCol < 0) {
-        toRow = 0;
-        toCol = 0;
-      }
+        console.log('left  : ' + cursorPosLeft);
+        console.log('offset: ' + invTableOffsetLeft);
+        var toRow = convertPosToDivArrayPos(cursorPosTop - invTableOffsetTop);
+        var toCol = convertPosToDivArrayPos(cursorPosLeft - invTableOffsetLeft);
+        if(toRow < 0 || toCol < 0 || toRow > 4 || toCol > 10) {
+          toRow = 0;
+          toCol = 0;
+        }
+        console.log("row: " + toRow);
+        console.log("col: " + toCol);
 
-      inventory.itemsGrid.forEach(function(row, fromRow) {
-        row.forEach(function(column, fromCol) {
-          if(column === itemId) {
-            var temp = inventory.itemsGrid[toRow][toCol];
-            inventory.itemsGrid[toRow][toCol] = inventory.itemsGrid[fromRow][fromCol];
-            inventory.itemsGrid[fromRow][fromCol] = temp;
-          }
+        inventory.itemsGrid.forEach(function(row, fromRow) {
+          row.forEach(function(column, fromCol) {
+            if(column === itemId) {
+              // console.log('it works: ' + column);
+              // console.log('fromRow: ' + fromRow);
+              // console.log('fromCol: ' + fromCol);
+              var temp = inventory.itemsGrid[toRow][toCol];
+              inventory.itemsGrid[toRow][toCol] = inventory.itemsGrid[fromRow][fromCol];
+              inventory.itemsGrid[fromRow][fromCol] = temp;
+            }
+          });
         });
-      });
 
-      Inventories.update(inventory._id, {$set: {itemsGrid: inventory.itemsGrid}});
+        $(event.target).css({'top': 1, 'left': 1});
+        Inventories.update(inventory._id, {$set: {itemsGrid: inventory.itemsGrid}});
 
-    }
-  });
+      }
+    });
   });
 };
 
@@ -118,5 +128,19 @@ Template.itemCell.helpers({
 
   item: function() {
     return Items.findOne({_id: this.toString()});
+  },
+  itemStats: function() {
+    var item = Items.findOne({_id: this.toString()});
+    var itemStats = "Name: " + item.name + "\nType:" + item.type + "\nMaterial:" + item.material +
+    "\nHlth:" + item.health + "\nStr:" + item.strength + "\nInt:" + item.intelligence + "\nQuantity:" + item.quantity +
+    "\nPrice:" + item.price;
+    return itemStats;
   }
 });
+
+Template.itemCell.rendered = function() {
+  $('[data-toggle="popover"]').popover({
+      trigger: 'hover',
+          'placement': 'top'
+  });
+};
