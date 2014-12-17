2ecd61ebf9fa06408673a9e50c1731654c01d3f3
Dominic Ho <domtkho@gmail.com>
2014-12-17 13:13:17 +0800

    Correctly displays group quest info.

--- client/templates/quests/quest_active.js
+++ client/templates/quests/quest_active.js
@@ -1,3 +1,33 @@
+Template.questActive.rendered = function () {
+
+  var characterId = Characters.findOne({userId: Meteor.userId()})._id;
+  var soloQuest = Quests.findOne({userId: Meteor.user()._id, status: "active"});
+  var groupQuest = Quests.findOne({groupId: Groups.findOne({members: characterId})._id, status: "active"});
+
+  soloQuestCountDown = function(){
+    var time_left = moment(soloQuest.startDate).add(soloQuest.questDuration, 'd') - moment();
+    var hours = Math.floor(time_left / (1000 * 60 * 60));
+    var minutes = Math.floor((time_left - (hours * (1000 * 60 * 60))) / (1000 * 60));
+    var seconds = Math.floor((time_left - (hours * (1000 * 60 * 60)) - (minutes * 1000 * 60)) / 1000);
+    Session.set('soloQuestRemainingTimePretty', hours + " hrs " + minutes + " min " + seconds + " sec");
+    Meteor.setTimeout(soloQuestCountDown, 500);
+  };
+
+
+  groupQuestCountDown = function(){
+    var time_left = moment(groupQuest.startDate).add(groupQuest.questDuration, 'd') - moment();
+    var hours = Math.floor(time_left / (1000 * 60 * 60));
+    var minutes = Math.floor((time_left - (hours * (1000 * 60 * 60))) / (1000 * 60));
+    var seconds = Math.floor((time_left - (hours * (1000 * 60 * 60)) - (minutes * 1000 * 60)) / 1000);
+    Session.set('groupQuestRemainingTimePretty', hours + " hrs " + minutes + " min " + seconds + " sec");
+    Meteor.setTimeout(groupQuestCountDown, 500);
+  };
+
+  soloQuestCountDown();
+  groupQuestCountDown();
+
+};
+
 Template.questActive.helpers({
 
   currentSoloQuest: function() {
@@ -19,14 +49,10 @@ Template.questActive.helpers({
     return Session.get('soloQuestRemainingTimePretty');
   },
 
-  groupQuestTimeRemaining: function() {
-    return Session.get('groupQuestRemainingTimePretty');
-  },
-
   currentGroupQuest: function() {
-    var characterId = Characters.findOne({userId: Meteor.userId()})._id;
-    var group = Groups.findOne({members: characterId});
-    return Quests.fineOne({groupId: group._id});
+    var character = Characters.findOne({userId: Meteor.userId()});
+    var group = Groups.findOne({members: character._id});
+    return Quests.findOne({groupId: group._id, status: "active"});
   },
 
   currentGroupMembers: function(){
@@ -40,42 +66,64 @@ Template.questActive.helpers({
   },
 
   currentGroupMonster: function(){
-    var characterId = Characters.findOne({userId: Meteor.userId()})._id;
-    var group = Groups.findOne({members: characterId});
-    var quest = Quests.fineOne({groupId: group._id});
+    var character = Characters.findOne({userId: Meteor.userId()});
+    var group = Groups.findOne({members: character._id});
+    var quest = Quests.findOne({groupId: group._id, status: "active"});
     return Monsters.findOne({_id: quest.monsterId});
+  },
+
+  currentGroupMonsterFullHealth: function() {
+    var character = Characters.findOne({userId: Meteor.userId()});
+    var group = Groups.findOne({members: character._id});
+    var quest = Quests.findOne({groupId: group._id, status: "active"});
+    var monster = Monsters.findOne({_id: quest.monsterId});
+    var subMonstersHealth = _.pluck(monster.subMonsters, "fullHealth");
+    var totalHealth = subMonstersHealth.reduce(function(a,b){
+      return a + b;
+    });
+    return totalHealth;
+  },
+
+  currentGroupXPReward: function() {
+    var character = Characters.findOne({userId: Meteor.userId()});
+    var group = Groups.findOne({members: character._id});
+    var quest = Quests.findOne({groupId: group._id, status: "active"});
+    var monster = Monsters.findOne({_id: quest.monsterId});
+    return monster.subMonsters[character._id].XP;
+  },
+
+  currentGroupGoldReward: function() {
+    var character = Characters.findOne({userId: Meteor.userId()});
+    var group = Groups.findOne({members: character._id});
+    var quest = Quests.findOne({groupId: group._id, status: "active"});
+    var monster = Monsters.findOne({_id: quest.monsterId});
+    var subMonstersGold = _.pluck(monster.subMonsters, "gold");
+    return monster.subMonsters[character._id].gold;
+  },
+
+  currentGroupMonsterHealthPercentage: function() {
+    var character = Characters.findOne({userId: Meteor.userId()});
+    var group = Groups.findOne({members: character._id});
+    var quest = Quests.findOne({groupId: group._id, status: "active"});
+    var monster = Monsters.findOne({_id: quest.monsterId});
+    var subMonstersHealth = _.pluck(monster.subMonsters, "fullHealth");
+    var totalHealth = subMonstersHealth.reduce(function(a,b){
+      return a + b;
+    });
+    return Math.floor(monster.health / totalHealth * 100);
+  },
+
+  groupQuestTimeRemaining: function() {
+    return Session.get('groupQuestRemainingTimePretty');
   }
 });
 
 Template.questActive.events({
-  'click .attack': function() {
-
+  'click .soloQuestAttack': function() {
     Meteor.call('questAttack', 'solo', function (error, result) {});
+  },
+
+  'click .groupQuestAttack': function() {
+    Meteor.call('questAttack', 'group', function (error, result) {});
   }
 });
-
-Template.questActive.rendered = function () {
-
-  var soloQuest = Quests.findOne({userId: Meteor.user()._id, status: "active"});
-  soloQuestCountDown = function(){
-    var time_left = moment(soloQuest.startDate).add(soloQuest.questDuration, 'd') - moment();
-    var hours = Math.floor(time_left / (1000 * 60 * 60));
-    var minutes = Math.floor((time_left - (hours * (1000 * 60 * 60))) / (1000 * 60));
-    var seconds = Math.floor((time_left - (hours * (1000 * 60 * 60)) - (minutes * 1000 * 60)) / 1000);
-    Session.set('soloQuestRemainingTimePretty', hours + " hrs " + minutes + " min " + seconds + " sec");
-    Meteor.setTimeout(soloQuestCountDown, 500);
-  };
-  soloQuestCountDown();
-
-  var group = Groups.findOne({members: characterId});
-  var groupQuest = Quests.fineOne({groupId: group._id});
-  groupQuestCountDown = function(){
-    var time_left = moment(groupQuest.startDate).add(groupQuest.questDuration, 'd') - moment();
-    var hours = Math.floor(time_left / (1000 * 60 * 60));
-    var minutes = Math.floor((time_left - (hours * (1000 * 60 * 60))) / (1000 * 60));
-    var seconds = Math.floor((time_left - (hours * (1000 * 60 * 60)) - (minutes * 1000 * 60)) / 1000);
-    Session.set('groupQuestRemainingTimePretty', hours + " hrs " + minutes + " min " + seconds + " sec");
-    Meteor.setTimeout(groupQuestCountDown, 500);
-  };
-  groupQuestCountDown();
-};
