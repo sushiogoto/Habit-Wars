e8d8708c88ac44c77acaa3d6700fec23779962fd
sushiogoto <yoshio.goto@gmail.com>
2015-01-19 18:42:36 +0800

    Improve styling of group quest box in rpg game pane template and allow inserts of chat msgs

--- client/templates/rpg/rpg_game_pane.js
+++ client/templates/rpg/rpg_game_pane.js
@@ -22,7 +22,22 @@ Template.rpgGamePane.events({
   },
   'click .joinGroup': function() {
     Meteor.call('joinGroup', "git_commit_target", function (error, result) {});
-  }
+  },
+  'click .attack': function() {
+    Meteor.call('questAttack', 'group', function (error, result) {});
+  },
+
+  'click .easy-group-quest-init': function() {
+    Meteor.call('groupQuestVoteInit', 'easy', function (error, result) {});
+  },
+
+  'click .medium-group-quest-init': function() {
+    Meteor.call('groupQuestVoteInit', 'medium', function (error, result) {});
+  },
+
+  'click .hard-group-quest-init': function() {
+    Meteor.call('groupQuestVoteInit', 'hard', function (error, result) {});
+  },
 
 });
 
